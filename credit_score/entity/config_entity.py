c8026c3782669d657d064785dc45e70cda05de0d
sumeet <maheshwarisumeet128@gmail.com>
2023-04-23 09:36:34 +0530

    Data Ingestion completed sucessfully.

--- credit_score/entity/config_entity.py
+++ credit_score/entity/config_entity.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 
 DataIngestionConfig = namedtuple("DataIngestionConfig",[
     "dataset_download_url",
+    "tgz_download_dir",
     "raw_data_dir",
     "ingested_train_dir",
     "ingested_test_dir"])
